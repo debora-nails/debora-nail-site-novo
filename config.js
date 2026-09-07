bc81c9d78cf4b6f925cf7c618cdd94ec676ca464
debora-nails <ds105292@gmail.com>
2026-09-07 12:35:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.SUPABASE_CONFIG={url:"",publishableKey:""};
+window.SUPABASE_CONFIG={url:"https://pkkylsrcmhznodcdhrqk.supabase.co",publishableKey:"sb_publishable_xobGJgHlxRbC5168HJB_NA_ebWYEnRd"};
